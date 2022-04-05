a647b389fd853efdbff588ade549e0d504914cbd
peejaypel <61832441+peejaypel@users.noreply.github.com>
2022-04-05 21:12:31 +0800

    added new model for users

---
+++ lib/models/user.dart
@@ -0,0 +1,11 @@
+class User{
+  final int userId;
+  final String username;
+  final String password;
+
+  const User({
+    required this.userId,
+    required this.username,
+    required this.password
+});
+}
