72790cb4cf7240ada57f5cf20a23e2c652ba9323
peejaypel <61832441+peejaypel@users.noreply.github.com>
2022-04-05 20:22:24 +0800

    updated login page

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,28 +1,70 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class Login extends StatelessWidget {
   const Login({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Row(
-            //Logo
-            children: [Text("LOGO")],
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          padding: EdgeInsets.symmetric(horizontal: 50, vertical: 30),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Row(
+                //Logo
+                children: [Text("LOGO")],
+              ),
+              // Spacer(
+              //   flex: 1,
+              // ),
+              Row(
+                children: [Text("Sign in")],
+              ),
+              Row(children: const [
+                Text(
+                  "Get started by signing in",
+                  style: TextStyle(fontSize: 30),
+                )
+              ]),
+              Container(
+                child: TextFormField(
+                  decoration: InputDecoration(
+                      labelText: "Email", hintText: "john@doe.com"),
+                ),
+              ),
+              Container(
+                child: TextFormField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    labelText: "Password",
+                    hintText: "*******",
+                  ),
+                ),
+              ),
+              SizedBox(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [TextButton(
+                  onPressed: null,
+                  child: Text("Login"),
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all<Color>(
+                        Colors.deepPurpleAccent),
+                    foregroundColor: MaterialStateProperty.all<Color>(
+                        Colors.white),
+                  ),
+                ),
+                ],),
+              ),
+              // Spacer(
+              //   flex: 5,
+              // ),
+            ],
           ),
-          Row(
-            children: [Text("Sign in")],
-          ),
-          Row(children: const [
-            Text(
-              "Get started by signing in",
-              style: TextStyle(fontSize: 30),
-            )
-          ]),
-        ],
+        ),
       ),
     );
   }
