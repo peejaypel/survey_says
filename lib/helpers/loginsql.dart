d24b588ab4602763ec137454f79fa8d8c7f0398c
peejaypel <61832441+peejaypel@users.noreply.github.com>
2022-04-06 21:29:51 +0800

    fixed LoginHelper initialization, added hasUser method

--- lib/helpers/loginsql.dart
+++ lib/helpers/loginsql.dart
@@ -6,43 +6,70 @@ import 'package:flutter/widgets.dart';
 
 import '../models/user.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  final database = openDatabase(
-    join(await getDatabasesPath(), 'surveysays_database.db'),
-    onCreate: (db, version) {
-      return db.execute(
-        'CREATE TABLE users('
-            'userId INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, '
-            'username TEXT, '
-            'password TEXT)',
-      );
-    },
-    version: 1,
-  );
+class LoginSQLHelper{
+  static Database? _database;
 
+  // make this a singleton class
+  LoginSQLHelper._privateConstructor();
+  static final LoginSQLHelper instance = LoginSQLHelper._privateConstructor();
+
+  Future<Database?> get database async {
+    if (_database != null) return _database;
+    // lazily instantiate the db the first time it is accessed
+    _database = await _initDatabase();
+    return _database;
+  }
+
+  _initDatabase() async {
+    String path = 'surveysays_database.db';
+    return await openDatabase(path,
+        version: 1,
+        onCreate: _onCreate);
+  }
+
+  Future _onCreate(Database db, int version) async {
+    await db.execute(
+      'CREATE TABLE users('
+          'userId INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, '
+          'username TEXT, '
+          'password TEXT)',
+    );
+    print("LoginSQL: TABLE CREATED");
+  }
+
+  //Tools
   Future<void> addUser(User user) async {
-    // Get a reference to the database.
     final db = await database;
-    await db.insert(
+    await db?.insert(
       'users',
       user.toMap(),
       conflictAlgorithm: ConflictAlgorithm.replace,
     );
   }
 
-  // A method that retrieves all the dogs from the dogs table.
+  //return all users as list
   Future<List<User>> users() async {
     final db = await database;
 
-    final List<Map<String, dynamic>> maps = await db.query('users');
+    final List<Map<String, dynamic>> maps = await db!.query('users');
 
     return List.generate(maps.length, (i) {
       return User(
-        userId: maps[i]['userId'],
         username: maps[i]['username'],
         password: maps[i]['password'],
       );
     });
   }
+
+  //check if user exists using username
+  Future<bool> hasUser(User user) async{
+    Database? db = await database;
+    print("username is " + user.username);
+    var queryResult = await db!.rawQuery('SELECT * FROM users WHERE username="${user.username}"');
+    print("ABCDEFG");
+    if (queryResult.isEmpty) return false;
+    return true;
+  }
 }
+
+
