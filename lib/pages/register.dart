20150b83a11e1d8c700f027f07d19fe1e1a0f303
peejaypel <61832441+peejaypel@users.noreply.github.com>
2022-04-06 21:30:30 +0800

    added Register functionality

--- lib/pages/register.dart
+++ lib/pages/register.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:survey_says/helpers/loginsql.dart';
+import 'package:survey_says/models/user.dart';
+import 'package:survey_says/pages/home.dart';
 
 class Register extends StatefulWidget {
   const Register({Key? key}) : super(key: key);
@@ -8,25 +11,81 @@ class Register extends StatefulWidget {
 }
 
 class _RegisterState extends State<Register> {
+  final LoginSQLHelper loginHelper = LoginSQLHelper.instance;
+  final usernameController = TextEditingController();
+  final passwordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.green,
+      backgroundColor: Colors.white,
       body: SafeArea(
         child: Container(
+          padding: EdgeInsets.symmetric(vertical: 20, horizontal: 50),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.start,
             children: [
-              Row(children: [
-                Text("Register"),
-              ],),
-              Row(children: [TextButton(
-                onPressed: () {
-                  Navigator.pop(context);
+              Spacer(flex: 1),
+              Row(
+                children: const [
+                  Text(
+                    "Register",
+                    style: TextStyle(
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+              Container(
+                child: TextFormField(
+                  controller: usernameController,
+                  decoration: const InputDecoration(
+                    labelText: "Name",
+                    hintText: "John Doe",
+                  ),
+                ),
+              ),
+              Container(
+                child: TextFormField(
+                  controller: passwordController,
+                  obscureText: true,
+                  decoration: const InputDecoration(
+                    labelText: "Password",
+                    hintText: "*******",
+                  ),
+                ),
+              ),
+              TextButton(
+                onPressed: () async {
+                  User user = User(
+                      username: usernameController.text,
+                      password: passwordController.text);
+                  if (await loginHelper.hasUser(user)) {
+                    print("has user!");
+                  } else {
+                    print("registered");
+                    Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(
+                            builder: (BuildContext context) => Home()));
+                    loginHelper.addUser(user);
+                  }
                 },
-                child: const Text("Already have an account?"),
-              )],),
+                child: Text("Register"),
+              ),
+              Row(
+                children: [
+                  TextButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: const Text("Already have an account?"),
+                  )
+                ],
+              ),
               Row(),
+              Spacer(flex: 1),
             ],
           ),
         ),
