2005afd1a3cc2b717747efd9fea6e41c083558b7
EderJuniorChua <61592296+EderJuniorChua@users.noreply.github.com>
2022-07-14 18:35:47 +0800

    renamed survey_says -> arwyn_cookbook

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:survey_says/main.dart';
+import 'package:arwyn_cookbook/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
